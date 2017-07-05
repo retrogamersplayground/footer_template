1b4d2af9021b2d3e7f03849834782dacd76b241a
Neil Floyd <neilfloyd99@gmail.com>
2017-07-05 20:19:39 +0000

    initial commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+ <div align="center" id="pageFooter">
+ <footer class="text-center">
+
+
+<div class="primary-color text-primary-color">
+<hr><h2>Visit Us on Social Media</h2>
+     <a href="#">Twitter</a>
+     <a href="#">Facebook</a>
+     <a href="#">Instagram</a>
+     <a href="#">Snapchat</a>
+</div>    
+</div>
+
+
+<div class="container-fluid divider-color accent-text">
+  
+     <center><h2> &copyName 2017</h2></center>
+     
+   </footer>
+   
+   </div> 
